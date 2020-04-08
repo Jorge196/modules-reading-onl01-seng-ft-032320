b35a7437485dd00485efd69d1cc701797f289aa0
Jorge Ramirez <joramirez96@gmail.com>
2020-04-08 20:22:53 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,15 +6,9 @@ class Dancer
   include FancyDance::InstanceMethods
 end
 
-  extend MetaDancing
-  include Dance
-end 
-
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-
-
 end
